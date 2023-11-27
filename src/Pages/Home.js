e43bbf9e33e7e9fe1be26329c2f1ffcd83bcb482
ERNEST CHRIS <ernestchris6@gmail.com>
2023-11-27 21:51:15 +0100

    usestate

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Home.css'
+import { useState } from 'react';
 
 const Home = () => {
+
+    const [blog, setBlog] = useState({
+        title : "Web Dev Classes", author : "Chris", body : "Lorem ipsum dolor", id : "1"
+    },
+    {
+        title : "How to code", author : "Emma", body : "Lorem ipsum dolor",  id : "2"
+    },
+    {
+        title : "Keeping up with programming", author : "Josh", body : "Lorem ipsum dolor",  id : "3"
+    })
+    
     return ( 
         <div className='home'>
-            <p>khhjb</p>
-            
+            <p>{blog.title}</p>
+            <p>{blog.body}</p>
+            <p><i>{blog.author}</i></p>
         </div>
      );
 }
