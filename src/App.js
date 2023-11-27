0dc9ae4e798a69f944c4506114d93bc1e1081a93
ERNEST CHRIS <ernestchris6@gmail.com>
2023-11-27 07:38:49 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Navbar from './Component/Navbar';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
 
 const App =() => {
   return (
     <div>
 
-      <Navbar />
         <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route path="/" element={<Home />}>Home</Route>
         </Routes>
         </BrowserRouter>
     </div>
   )
 }
 
-export default App;
+export default App;     
