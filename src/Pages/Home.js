fe7d6722b0cd7d7bf63fd6c2108e7e510c2e9ab8
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-08 23:35:27 +0100

    hooks

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,9 +8,7 @@ const Home = () => {
 
     // const [isPending, setIsPending] = useState(false);
 
-      const [blogs, setBlogs] = useState(null);
-      const [isPending, setIsPending] = useState(true);
-      const [error, setError] = useState(null);
+     
 
     const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -20,32 +18,7 @@ const Home = () => {
     
     //Useffect hook
 
-    useEffect(()=> {
-        setTimeout(() => {
-         fetch(' http://localhost:8000/bloggs')
-         .then(res => {
-            if(!res.ok) {
-               throw Error('Could not fetch data!!!')
-            }
-            return res.json()
-            // console.log(res);
-         })
-         .then(data => {
-            setBlogs(data)
-            setIsPending(false);
-            setError(null)
-         })
-         .catch(err => {
-            console.log(err.message);
-            setError(err.message)
-            setIsPending(false)
-         })
-            
-        }, 2000);
-    }, []);
-
-
-
+    
     return ( 
         <div className='home'>
             {error && <div>{error}</div>}
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,8 +1,35 @@
+import {useEffect, useState} from 'react'
+
 const useFetch = () => {
+
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
     return ( 
-        <div>
-            
-        </div>
+        useEffect(()=> {
+            setTimeout(() => {
+             fetch(' http://localhost:8000/bloggs')
+             .then(res => {
+                if(!res.ok) {
+                   throw Error('Could not fetch data!!!')
+                }
+                return res.json()
+                // console.log(res);
+             })
+             .then(data => {
+                setBlogs(data)
+                setIsPending(false);
+                setError(null)
+             })
+             .catch(err => {
+                console.log(err.message);
+                setError(err.message)
+                setIsPending(false)
+             })
+                
+            }, 2000);
+        }, [])
      );
 }
  
