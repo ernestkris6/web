a67fbf479bea7bade618e5d7fc216172b0ce9598
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-09 23:19:47 +0100

    fetch data

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,14 +2,41 @@ import React, {useEffect, useState} from 'react';
 import './Home.css'
 
 import Blog from '../Blog';
-import useFetch from '../useFetch';
+// import useFetch from '../useFetch';
 
 
 const Home = () => {
 
     // const [isPending, setIsPending] = useState(false);
    
-     
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(()=> {
+        setTimeout(() => {
+         fetch('http://lovalhost:8000/blogs')
+         .then(res => {
+            if(!res.ok) {
+               throw Error('Could not fetch data!!!')
+            }
+            return res.json()
+            
+         })
+         .then(data => {
+            setBlogs(data)
+            setIsPending(false);
+            setError(null)
+         })
+         .catch(err => {
+            console.log(err.message);
+            setError(err.message)
+            setIsPending(false)
+         })
+            
+        }, 2000);
+    }, [])
+
+
 
    //  const handleDelete = (id) => {
    //  const newBlogs = blogs.filter(blog => blog.id !== id);
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,38 +1,14 @@
-import {useEffect, useState} from 'react'
+// import {useEffect, useState} from 'react'
 
-const useFetch = (url) => {
+// const useFetch = (url) => {
 
-    const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+   
+//     return ( 
+//         <div>
 
-    return ( 
-        useEffect(()=> {
-            setTimeout(() => {
-             fetch(url)
-             .then(res => {
-                if(!res.ok) {
-                   throw Error('Could not fetch data!!!')
-                }
-                return res.json()
-                console.log(res);
-             })
-             .then(data => {
-                setData(data)
-                setIsPending(false);
-                setError(null)
-             })
-             .catch(err => {
-                console.log(err.message);
-                setError(err.message)
-                setIsPending(false)
-             })
-                
-            }, 2000);
-        }, [url])
-     );
+//         </div>
+//      );
 
-     return {data, isPending, error}
-}
+// }
  
-export default useFetch;
+// export default useFetch;
