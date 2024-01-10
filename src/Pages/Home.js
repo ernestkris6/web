8152a7c1e072cfd90fc4f6203afda1edca1aad50
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-10 20:34:28 +0100

    home/usefetch

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,30 +12,7 @@ const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
-    useEffect(()=> {
-        setTimeout(() => {
-         fetch('http://localhost:8000/blogs')
-         .then(res => {
-            if(!res.ok) {
-               throw Error('Could not fetch data!!!')
-            }
-            return res.json()
-            
-         })
-         .then(data => {
-            setBlogs(data)
-            setIsPending(false);
-            setError(null)
-         })
-         .catch(err => {
-            console.log(err.message);
-            setError(err.message)
-            setIsPending(false)
-         })
-            
-        }, 2000);
-    }, [])
-
+    
 
 
    //  const handleDelete = (id) => {
--- src/useFetch.js
+++ src/useFetch.js
@@ -4,9 +4,30 @@ const useFetch = (url) => {
 
    
     return ( 
-        <div>
-
-        </div>
+        useEffect(()=> {
+            setTimeout(() => {
+             fetch('http://localhost:8000/blogs')
+             .then(res => {
+                if(!res.ok) {
+                   throw Error('Could not fetch data!!!')
+                }
+                return res.json()
+                
+             })
+             .then(data => {
+                setBlogs(data)
+                setIsPending(false);
+                setError(null)
+             })
+             .catch(err => {
+                console.log(err.message);
+                setError(err.message)
+                setIsPending(false)
+             })
+                
+            }, 2000);
+        }, [])
+    
      );
 
 }
