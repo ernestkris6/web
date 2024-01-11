dd4b3ee724a028c2c666243635fec9c1a0dd9042
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-11 22:29:30 +0100

    blog page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Component/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Create from './Pages/Create';
+import Blog from './Blog';
 
 const App =() => {
   return (
@@ -13,7 +14,7 @@ const App =() => {
         <Routes>
           <Route exact path="/" element={<Home />}>Home</Route>
           <Route path='/create' element={<Create />}></Route>
-
+          <Route path='/blog' element={<Blog />}></Route>
         </Routes>
         </BrowserRouter>
     </div>
