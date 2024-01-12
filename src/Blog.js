9c8a3e0e6ccee0a4187292c093fcd2b620bab6e7
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-12 18:39:56 +0100

    blog details id

--- src/Blog.js
+++ src/Blog.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Blog.css';
+import { Link } from 'react-router-dom';
 
 
 const Blog = ({blogs, title, author, body}) => {
@@ -10,8 +11,10 @@ const Blog = ({blogs, title, author, body}) => {
         <h1> {title}</h1>
              {blogs.map((blog) => (
             <div className='blog-list' key={blog.id}>
+                <Link to={`blogs/blog ${blog.id}`}>
                 <h2>{blog.title}</h2>
                 <p>{blog.body}</p>
+                </Link>
                 <h4><i>{blog.author}</i></h4>
 
                 <button>DELETE</button>
