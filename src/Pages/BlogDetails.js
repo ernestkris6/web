87a290e4205c184d08310f5f02640599b3f6d4b2
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-13 23:51:37 +0100

    err could not fetch

--- src/Pages/BlogDetails.js
+++ src/Pages/BlogDetails.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import './BlogDetails.css';
 import useFetch from '../useFetch';
+
 const BlogDetails = () => {
     const { id } = useParams();
     const {data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id);
@@ -8,7 +9,7 @@ const BlogDetails = () => {
 
     return ( 
         <div className="blog-details">
-            <h1>Blog Details - { id }</h1>
+            {/* <h1>Blog Details - { id }</h1> */}
             { isPending && <div>Loading...</div> }
             { error && <div>{ error }</div> }
             { blog && (
