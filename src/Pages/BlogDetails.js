26e6f25b6ef8124763c597d86030629b29635957
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-13 23:41:20 +0100

    blog data

--- src/Pages/BlogDetails.js
+++ src/Pages/BlogDetails.js
@@ -3,11 +3,15 @@ import './BlogDetails.css';
 import useFetch from '../useFetch';
 const BlogDetails = () => {
     const { id } = useParams();
-    const {data, isPending, error} = useFetch('http://localhost:8000/blogs')
+    const {data, isPending, error} = useFetch('http://localhost:8000/blogs/' + id);
+
 
     return ( 
         <div className="blog-details">
             <h1>Blog Details - { id }</h1>
+            { isPending && <div>Loading...</div>}
+            {error && <div>{ error }</div>}
+            { data }
         </div>
      );
 }
