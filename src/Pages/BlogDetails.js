9bb05ace220a90153d203a1790afa9dc9ef73b45
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-13 23:37:45 +0100

    blog details

--- src/Pages/BlogDetails.js
+++ src/Pages/BlogDetails.js
@@ -1,8 +1,9 @@
 import { useParams } from 'react-router-dom';
 import './BlogDetails.css';
-
+import useFetch from '../useFetch';
 const BlogDetails = () => {
     const { id } = useParams();
+    const {data, isPending, error} = useFetch('http://localhost:8000/blogs')
 
     return ( 
         <div className="blog-details">
