e963f9fabce0e0545a7adcb7cc8b9b87241d35a2
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-15 23:17:51 +0100

    could not fetch

--- src/Pages/BlogDetails.js
+++ src/Pages/BlogDetails.js
@@ -4,7 +4,7 @@ import useFetch from '../useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
-    const {data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id);
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs/' + id);
 
 
     return ( 
@@ -12,11 +12,11 @@ const BlogDetails = () => {
             <h1>Blog Details - { id }</h1>
             { isPending && <div>Loading...</div> }
             { error && <div>{ error }</div> }
-            { blog && (
+            { blogs && (
                 <article>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by {blog.author} </p>
-                    <div>{ blog.body }</div>
+                    <h2>{ blogs.title }</h2>
+                    <p>Written by {blogs.author} </p>
+                    <div>{ blogs.body }</div>
                 </article>
             ) }
         </div>
