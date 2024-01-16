9b45887bb25989f5d9bc719481fcb49b49c36513
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-16 23:40:07 +0100

    label

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -4,6 +4,14 @@ const Create = () => {
     return ( 
         <div className="create">
             <h1>Add a new blog</h1>
+
+            <form>
+                <label>Blog Title:</label>
+                <input 
+                type='text' 
+                required 
+                />
+            </form>
         </div>     
         );
 }
