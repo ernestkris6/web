a97e8810b98bd70598dd698d0814540b684e8876
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-21 00:13:25 +0100

    handle click

--- src/Pages/BlogDetails.js
+++ src/Pages/BlogDetails.js
@@ -1,16 +1,22 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate} from 'react-router-dom';
 import './BlogDetails.css';
 import useFetch from '../useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
     const {data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id);
-    
-    const handleDelete = () => {
-        fetch('http://localhost:8000/blogs/' + blog.id {
-            method : "DELETE"
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: "DELETE"
+            })
+        .then(() => {
+            navigate('/');
         })
     }
+
+
     return ( 
         <div className="blog-details">
             <h1>Blog Details - { id }</h1>
@@ -21,7 +27,7 @@ const BlogDetails = () => {
                     <h3>{ blog.title }</h3>
                 <p><b><i>Written by { blog.author }</i></b></p> <br></br>
                <div><h3>{ blog.body }</h3></div> 
-               <button onClick={handleDelete}>DELETE</button>
+               <button onClick={handleClick}>DELETE</button>
                 </article>
             ) }
         </div>
